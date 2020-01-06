e6d606f708d8a99cc40a118e632d3b6f52955b22
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-01-06 16:18:33 +0200

    feat: mode

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require("webpack")
+const HtmlWEbPackPlugin = require("html-webpack-plugin")
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node-module/,
+                loader: "baabel-loader"
+            }
+        ]
+    },
+    plugins: [
+
+        new HtmlWEbPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+
+    ]
+}
